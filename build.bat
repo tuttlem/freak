0a757a6fe23ba6c3612b019808ddbb1e2eaffbf2
Michael Tuttle <tuttlem@gmail.com>
2015-10-28 08:08:14 +1000

    Split build scripts

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
 
 @echo off
 
-del bin\*.*
-del *.obj
-del *.exe
-
 call wcc386 /d2 /3s src\util.c -fo=bin\util.obj
 IF ERRORLEVEL 1 GOTO DONE
 
@@ -18,18 +14,6 @@ call wcc386 /d2 /3s src\poly.c -fo=bin\poly.obj
 IF ERRORLEVEL 1 GOTO DONE
 
 call wcc386 /d2 /3s src\freak.c -fo=bin\freak.obj
-IF ERRORLEVEL 1 GOTO DONE
-
-call wcc386 /d2 /3s test\test.c -fo=bin\test.obj
-IF ERRORLEVEL 1 GOTO DONE
-
-call wcc386 /d2 /3s test\test2.c -fo=bin\test2.obj
-IF ERRORLEVEL 1 GOTO DONE
-
-call wlink system dos4g file bin\test,bin\freak,bin\util,bin\vga,bin\poly,bin\fixed name test
-IF ERRORLEVEL 1 GOTO DONE
-
-call wlink system dos4g file bin\test2,bin\freak,bin\util,bin\vga,bin\poly,bin\fixed name test2
 IF ERRORLEVEL 1 GOTO DONE
 
 :DONE
---
+++ clean.bat
@@ -0,0 +1,7 @@
+
+@echo off
+
+del bin\*.*
+del *.obj
+del *.exe
+
